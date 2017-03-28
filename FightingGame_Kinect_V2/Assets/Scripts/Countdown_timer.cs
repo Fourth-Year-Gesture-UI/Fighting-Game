0c16fc5a8cae031a1a4f4ba9d2eda0c9d618477f
Christy <christymadden80@gmail.com>
2017-03-28 13:01:01 +0100

    Countdown timer added

---
+++ FightingGame_Kinect_V2/Assets/Scripts/Countdown_timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown_timer : MonoBehaviour {
+
+    float timeLeft = 90.0f;
+
+    public Text text;
+
+    void Update()
+    {
+
+        timeLeft -= Time.deltaTime;
+        text.text =  Mathf.Round(timeLeft).ToString();
+       
+    }
+}
